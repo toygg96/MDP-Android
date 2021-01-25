b32d828ebfdaf0d57ca34aff23c03d574d614c0a
toygg96 <jose0039@e.ntu.edu.sg>
2021-01-25 23:39:00 +0800

    Added logo for action bar and application
    - Added logo for action bar
    - Added logo for application

--- app/src/main/java/com/example/mdp/MainActivity.java
+++ app/src/main/java/com/example/mdp/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter BA;
     private Set<BluetoothDevice> pairedDevices;
     private ArrayAdapter adapter;
-    private Set bluetoothDevices = new HashSet();
     private ListView lv;
     private TextView bluetoothStatusTextView;
 
@@ -38,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().setLogo(R.drawable.robot);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         onBluetoothBtn = (Button) findViewById(R.id.onBluetoothBtn);
         makeVisibleBtn=(Button)findViewById(R.id.makeVisibleBtn);
         offBluetoothBtn=(Button)findViewById(R.id.offBluetoothBtn);
